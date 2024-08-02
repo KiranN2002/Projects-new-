2d97afcb68759ebfbc47849e87e3b03034c0bbdd
KiranN2002 <147976235+KiranN2002@users.noreply.github.com>
2024-08-02 12:36:11 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './First'
+
+const myCar = {
+name:'BMW',
+color:'black',
+year:'2020'
+}
+
+function App() {
+  return (
+    <div className="App">
+    <First/>
+    </div>
+  );
+}
+
+export default App;
